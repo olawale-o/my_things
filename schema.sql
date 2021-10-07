b223cf5e59100e859acfa88e9c4b64f89f3fa3ad
olawale-o <omoogunolawale@gmail.com>
2021-10-07 13:09:57 +0100

    Create books table

--- schema.sql
+++ schema.sql
@@ -25,3 +25,33 @@ CREATE TABLE labels(
   color VARCHAR(100),
   PRIMARY KEY(id)
 );
+
+CREATE TABLE books(
+  id INT UNIQUE GENERATED ALWAYS AS IDENTITY,
+  genre_id INT,
+  author_id INT,
+  source_id INT,
+  label_id INT,
+  publish_date DATE,
+  archived BOOLEAN DEFAULT false,
+  publisher VARCHAR(100),
+  cover_state VARCHAR(100),
+  PRIMARY KEY(id),
+  CONSTRAINT fk_genres
+  FOREIGN KEY(genre_id)
+  REFERENCES genres(id),
+  CONSTRAINT fk_authors
+  FOREIGN KEY(author_id)
+  REFERENCES authors(id)
+  CONSTRAINT fk_sources
+  FOREIGN KEY(source_id)
+  REFERENCES sources(id)
+  CONSTRAINT fk_labels
+  FOREIGN KEY(label_id)
+  REFERENCES labels(id)
+);
+
+CREATE INDEX idx_books_genre_id ON books (genre_id);
+CREATE INDEX idx_books_auhor_id ON books (author_id);
+CREATE INDEX idx_books_source_id ON books (source_id);
+CREATE INDEX idx_books_label_id ON books (label_id);
