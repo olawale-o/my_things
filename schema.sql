53a0899feb74b59612fbe785811ac617a99656aa
olawale-o <omoogunolawale@gmail.com>
2021-10-07 13:14:43 +0100

    Add movies table

--- schema.sql
+++ schema.sql
@@ -84,3 +84,32 @@ CREATE INDEX idx_music_albums_genre_id ON books (genre_id);
 CREATE INDEX idx_music_albums_auhor_id ON books (author_id);
 CREATE INDEX idx_music_albums_source_id ON books (source_id);
 CREATE INDEX idx_music_albums_label_id ON books (label_id);
+
+CREATE TABLE movies(
+  id INT UNIQUE GENERATED ALWAYS AS IDENTITY,
+  genre_id INT,
+  author_id INT,
+  source_id INT,
+  label_id INT,
+  publish_date DATE,
+  archived BOOLEAN DEFAULT false,
+  silet BOOLEAN DEFAULT false,
+  PRIMARY KEY(id),
+  CONSTRAINT fk_genres
+  FOREIGN KEY(genre_id)
+  REFERENCES genres(id),
+  CONSTRAINT fk_authors
+  FOREIGN KEY(author_id)
+  REFERENCES authors(id)
+  CONSTRAINT fk_sources
+  FOREIGN KEY(source_id)
+  REFERENCES sources(id)
+  CONSTRAINT fk_labels
+  FOREIGN KEY(label_id)
+  REFERENCES labels(id)
+);
+
+CREATE INDEX idx_movies_genre_id ON books (genre_id);
+CREATE INDEX idx_movies_auhor_id ON books (author_id);
+CREATE INDEX idx_movies_source_id ON books (source_id);
+CREATE INDEX idx_movies_label_id ON books (label_id);
