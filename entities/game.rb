402044fe0d4a49ac2f6d7ae53b3f759f0bfe1431
olawale-o <omoogunolawale@gmail.com>
2021-10-07 13:53:35 +0100

    Create game entity

---
+++ entities/game.rb
@@ -0,0 +1,13 @@
+require_relative 'item'
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(multiplayer, last_played_at, args)
+    super(args)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?; end
+end
