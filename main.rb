6b50255cc4eb3471e75d5d754118a5d752812b3d
olawale-o <omoogunolawale@gmail.com>
2021-10-14 09:34:56 +0100

    Create Screen module

---
+++ main.rb
@@ -0,0 +1,11 @@
+class App
+  def execute
+  end
+end
+
+def main
+  app = App.new
+  app.execute
+end
+
+main
---
+++ modules/screen.rb
@@ -0,0 +1,9 @@
+module Screen
+  def user_input
+    gets.chomp
+  end
+
+  def console(text)
+    puts text
+  end
+end
